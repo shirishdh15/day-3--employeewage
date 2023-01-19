c5ab75c1e7e6c2e9dae1d9c9b28903b24c3548bf
shirish <shirishdhalgade15@gmail.com>
2023-01-19 17:13:00 +0530

    UC4

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,12 +3,11 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
-        Random attendance =new Random();
+        Random attendance = new Random();
         int atndc = attendance.nextInt(2);
-        if(atndc ==0) {
+        if (atndc == 0) {
             System.out.println("employee is absent");
-        }
-        else {
+        } else {
             System.out.println("employee is present");
 
             int wagePerHour = 20;
@@ -20,7 +19,15 @@ public class EmployeeWage {
             int partTimeHour = 8;
             int partimeWage = (partTimeWage * partTimeHour);
             System.out.println("Part time wage is  " + partimeWage);
+
+            switch (atndc) {
+                case 0:
+                    System.out.println("Absent");
+                    break;
+                case 1:
+                    System.out.println("Present");
+                    break;
+            }
         }
     }
 }
-
