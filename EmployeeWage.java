bbd84aa2b89f3cb6a1259a9f1f6dd09ea6eaa92d
shirish <shirishdhalgade15@gmail.com>
2023-01-19 13:35:34 +0530

    Employeewage

---
+++ EmployeeWage.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+
+        Random attendance =new Random();
+        int atndc = attendance.nextInt(2);
+        if(atndc ==0) {
+            System.out.println("employee is absent");
+        }
+        else {
+            System.out.println("employee is present");
+        }
+    }
+}
+
