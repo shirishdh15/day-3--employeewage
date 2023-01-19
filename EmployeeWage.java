ed81c253bdae6fb09e1b05049007bf6638fbaa09
shirish <shirishdhalgade15@gmail.com>
2023-01-19 13:48:19 +0530

    UC2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,6 +10,11 @@ public class EmployeeWage {
         }
         else {
             System.out.println("employee is present");
+
+            int wagePerHour = 20;
+            int fullDay = 8;
+            int dailyWage = (wagePerHour * fullDay);
+            System.out.println("Daily employee Wage is  " + dailyWage);
         }
     }
 }
